024e49414bfa157d54727d1ccc461f0c52f74fe7
DESKTOP-6CN4A1N\USER <bandyopadhyay.bhaskar81@gmail.com>
2020-10-06 15:37:42 +0530

    test

--- checktimezone.php
+++ checktimezone.php
@@ -1,5 +1,5 @@
 <?php
-echo 333;exit;//
+echo 333999;exit;//
 $user_ip = getenv('REMOTE_ADDR'); 
 ////$geo = file_get_contents("http://api.ipstack.com/".$_SERVER['REMOTE_ADDR']."?access_key=09111af3ea5a4fc0c10d12dfdabe77b9"); 
 ////$geo = file_get_contents("http://api.ipstack.com/223.191.16.14?access_key=09111af3ea5a4fc0c10d12dfdabe77b9"); 
